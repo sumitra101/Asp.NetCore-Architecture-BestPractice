d3012548e57651acd292e593644c213e33cd48fc
Sumitra <skhadka@qkly.io>
2022-05-01 22:25:30 +0545

    Add project files.

---
+++ EventTicket.TicketManagement.Domain/Entities/Category.cs
@@ -0,0 +1,14 @@
+﻿using EventTicket.TicketManagement.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventTicket.TicketManagement.Domain.Entities
+{
+    public class Category : AuditableEntity
+    {
+        public Guid CategoryId { get; set; }
+        public string Name { get; set; }
+        public ICollection<Event> Events { get; set; }
+    }
+}
