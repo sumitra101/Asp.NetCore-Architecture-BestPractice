9dfbc23109d8807a6c141dc8aa8d3bbd417eb870
Sumitra <skhadka@qkly.io>
2022-05-02 10:05:00 +0545

    Add interface for category, event and order

---
+++ EventTicket.TicketManagement.Application/Contracts/Persistence/ICategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using EventTicket.TicketManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventTicket.TicketManagement.Application.Contracts.Persistence
+{
+    internal interface ICategoryRepository : IAsyncRepository<Category>
+    {
+    }
+}
---
+++ EventTicket.TicketManagement.Application/Contracts/Persistence/IOrderRepository.cs
@@ -0,0 +1,11 @@
+﻿using EventTicket.TicketManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventTicket.TicketManagement.Application.Contracts.Persistence
+{
+    public interface IOrderRepository : IAsyncRepository<Order>
+    {
+    }
+}
