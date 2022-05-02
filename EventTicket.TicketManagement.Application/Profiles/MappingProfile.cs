ff294854f5ef9f6a0f95ec4b31fdb3a587ea8592
Sumitra <skhadka@qkly.io>
2022-05-02 10:06:12 +0545

    Create Mapping profile to mapp view model and entity

---
+++ EventTicket.TicketManagement.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using EventTicket.TicketManagement.Application.Features.Events;
+using EventTicket.TicketManagement.Application.Features.Events.Models;
+using EventTicket.TicketManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventTicket.TicketManagement.Application.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Event, EventListVm>().ReverseMap();
+            CreateMap<Event, EventDetailVm>().ReverseMap();
+            CreateMap<Category, CategoryDto>();
+
+        }
+    }
+}
