ecbbf9d4cf2bce1a8794cc941b53c75c6c9452fe
Sumitra <skhadka@qkly.io>
2022-05-02 10:05:51 +0545

    Create query for getting event list

---
+++ EventTicket.TicketManagement.Application/Features/Events/Queries/GetEventList/GetEventListQuery.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using EventTicket.TicketManagement.Application.Contracts.Persistence;
+using EventTicket.TicketManagement.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EventTicket.TicketManagement.Application.Features.Events.Queries.GetEventList
+{
+    public class GetEventListQuery : IRequest<List<EventListVm>>
+    {
+    }
+
+    public class GetEventListQueryHandler : IRequestHandler<GetEventListQuery, List<EventListVm>>
+    {
+        private readonly IAsyncRepository<Event> _eventRepository;
+        private readonly IMapper _mapper;
+        public GetEventListQueryHandler(IAsyncRepository<Event> eventRepository,IMapper mapper)
+        {
+            _mapper = mapper;
+            _eventRepository = eventRepository;
+
+        }
+        public async Task<List<EventListVm>> Handle(GetEventListQuery request, CancellationToken cancellationToken)
+        {
+            var allEvents = (await _eventRepository.ListAllAsync()).OrderBy(x => x.Date);
+            return _mapper.Map<List<EventListVm>>(allEvents);
+        }
+    }
+}
