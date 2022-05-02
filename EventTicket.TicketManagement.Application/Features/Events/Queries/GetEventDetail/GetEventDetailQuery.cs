285827a167c2f190ee019382c559f59f89e8309c
Sumitra <skhadka@qkly.io>
2022-05-02 10:05:34 +0545

    Create query for getting event detail

---
+++ EventTicket.TicketManagement.Application/Features/Events/Queries/GetEventDetail/GetEventDetailQuery.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using EventTicket.TicketManagement.Application.Contracts.Persistence;
+using EventTicket.TicketManagement.Application.Features.Events.Models;
+using EventTicket.TicketManagement.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EventTicket.TicketManagement.Application.Features.Events.Queries.GetEventDetail
+{
+    public class GetEventDetailQuery : IRequest<EventDetailVm>
+    {
+        public Guid Id { get; set; }
+    }
+
+    public class GetEventDetailQueryHandler : IRequestHandler<GetEventDetailQuery, EventDetailVm>
+    {
+        private readonly IAsyncRepository<Event> _eventRepository;
+        private readonly IAsyncRepository<Category> _categoryRepository;
+        private readonly IMapper _mapper;
+        public GetEventDetailQueryHandler(IAsyncRepository<Event> eventRepository,IAsyncRepository<Category> categoryRepository,
+            IMapper mapper)
+        {
+            _eventRepository = eventRepository;
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+        public async Task<EventDetailVm> Handle(GetEventDetailQuery request, CancellationToken cancellationToken)
+        {
+            var @event = await _eventRepository.GetByIdAsync(request.Id);
+            var eventDetailDto=_mapper.Map<EventDetailVm>(@event);
+
+            var category=await _categoryRepository.GetByIdAsync(@event.CategoryId);
+            eventDetailDto.Category = _mapper.Map<CategoryDto>(category);
+            return eventDetailDto;
+        }
+    }
+}
